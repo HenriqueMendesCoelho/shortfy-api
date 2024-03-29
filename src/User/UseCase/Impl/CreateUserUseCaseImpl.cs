a898458d078bd514c4fb55650a1867225446c294
HenriqueMendesCoelho <henriquemendescoelho@gmail.com>
2024-03-29 02:58:32 -0300

    refactor: Assign 'ADMIN' role to the first created user

--- src/User/UseCase/Impl/CreateUserUseCaseImpl.cs
+++ src/User/UseCase/Impl/CreateUserUseCaseImpl.cs
@@ -6,17 +6,30 @@ namespace suavesabor_api.User.UseCase.Impl
 {
     public class CreateUserUseCaseImpl(IUserRepository repository) : ICreateUserUseCase
     {
+        private static readonly UserRoleDomain ADMIN_ROLE = new() { Role = RoleDomain.ADMIN };
         private static readonly UserRoleDomain USER_ROLE = new() { Role = RoleDomain.USER };
 
         private readonly IUserRepository _repository = repository;
 
         async public Task<UserDomain> Create(UserDomain user)
         {
-            user.Roles = [USER_ROLE];
+
+            user.Roles = await ExistsAnyUser() ? [USER_ROLE] : [ADMIN_ROLE, USER_ROLE];
             user.CreatedAt = DateTime.UtcNow;
             user.Password = EncryptPassword(user.Password);
 
             return await _repository.Create(user);
+        }
+
+        async private Task<bool> ExistsAnyUser()
+        {
+            var usuarios = await _repository.FindAll();
+            if (usuarios is null || usuarios.Count == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         private string EncryptPassword(string password)
